9323151d85649861baac3496d1424d4ddbf50b7e
HiveProject <jwang123@nyit.edu>
2022-08-08 14:04:39 -0700

    Update UnitValue.ts

--- chives-blockchain-gui/packages/core/src/constants/UnitValue.ts
+++ chives-blockchain-gui/packages/core/src/constants/UnitValue.ts
@@ -2,8 +2,8 @@ import Unit from './Unit';
 
 const UnitValue = {
   [Unit.CHIVES]: 1,
-  [Unit.MOJO]: 1 / 1e12,
-  [Unit.CAT]: 1 / 1e9,
+  [Unit.MOJO]: 1 / 1e8,
+  [Unit.CAT]: 1 / 1e5,
 };
 
 export default UnitValue;
