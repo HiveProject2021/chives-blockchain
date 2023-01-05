65b0fedb104af82d02a12f513edbe15a3a7ebfad
hivecoin2021@gmail.com <hivecoin2021@gmail.com>
2023-01-04 22:36:41 -0800

    masternode as a service

--- chives/seeder/start_masternode.py
+++ chives/seeder/start_masternode.py
@@ -1,3 +1,5 @@
+import asyncio
+import time
 import logging
 import pathlib
 from multiprocessing import freeze_support
@@ -5,13 +7,16 @@ from typing import Dict
 
 from chives.consensus.constants import ConsensusConstants
 from chives.consensus.default_constants import DEFAULT_CONSTANTS
-from chives.rpc.crawler_rpc_api import CrawlerRpcApi
-from chives.seeder.crawler import Crawler
-from chives.seeder.crawler_api import CrawlerAPI
 from chives.server.outbound_message import NodeType
 from chives.server.start_service import run_service
 from chives.util.config import load_config_cli
+from chives.util.config import load_config
 from chives.util.default_root import DEFAULT_ROOT_PATH
+from chives.util.chives_logging import initialize_logging
+import datetime
+import requests
+import json
+
 
 # See: https://bugs.python.org/issue29288
 "".encode("idna")
@@ -20,44 +25,38 @@ SERVICE_NAME = "full_node"
 log = logging.getLogger(__name__)
 
 
-def service_kwargs_for_full_node_crawler(
-    root_path: pathlib.Path, config: Dict, consensus_constants: ConsensusConstants
-) -> Dict:
-    crawler = Crawler(
-        config,
-        root_path=root_path,
-        consensus_constants=consensus_constants,
-    )
-    api = CrawlerAPI(crawler)
-
-    network_id = config["selected_network"]
-    kwargs = dict(
-        root_path=root_path,
-        node=api.crawler,
-        peer_api=api,
-        node_type=NodeType.FULL_NODE,
-        advertised_port=config["port"],
-        service_name=SERVICE_NAME,
-        upnp_ports=[],
-        server_listen_ports=[config["port"]],
-        on_connect_callback=crawler.on_connect,
-        network_id=network_id,
-    )
-
-    if config.get("crawler", {}).get("start_rpc_server", True):
-        kwargs["rpc_info"] = (CrawlerRpcApi, config.get("crawler", {}).get("rpc_port", 9561))
-
-    return kwargs
-
-
+async def masternode_update_status_interval(selected_network, config) -> None:
+    while True:
+        x = datetime.datetime.now()
+        log.warning("*"*64)
+        log.warning(f"[{selected_network}] Masternode heartbeat {x} ")
+        MasterNodeHeartBeat = {}
+        MasterNodeHeartBeat['selected_network'] = selected_network
+        MasterNodeHeartBeat['Address'] = "Address"
+        MasterNodeHeartBeat['MasterNodeId'] = "MasterNodeId"
+        MasterNodeHeartBeat['FingerPrint'] = "FingerPrint"
+        MasterNodeHeartBeatText = json.dumps(MasterNodeHeartBeat, indent=4, sort_keys=True)
+        try:
+            content = requests.post('https://community.chivescoin.org/masternode/', data={'MasterNodeHeartBeat':MasterNodeHeartBeatText}, timeout=3)
+            log.warning(content.text) 
+        except requests.exceptions.ConnectionError:
+            log.warning('ConnectionError -- please wait 600 seconds............................')
+        except requests.exceptions.ChunkedEncodingError:
+            log.warning('ChunkedEncodingError -- please wait 600 seconds............................')
+        except:
+            log.warning('Unfortunitely -- An Unknow Error Happened, Please wait 600 seconds............................')
+        
+        time.sleep(10)
+        
 def main():
     config = load_config_cli(DEFAULT_ROOT_PATH, "config.yaml", "seeder")
-    overrides = config["network_overrides"]["constants"][config["selected_network"]]
-    updated_constants = DEFAULT_CONSTANTS.replace_str_to_bytes(**overrides)
-    kwargs = service_kwargs_for_full_node_crawler(DEFAULT_ROOT_PATH, config, updated_constants)
-    return run_service(**kwargs)
+    selected_network = config["selected_network"]
+    root_path = DEFAULT_ROOT_PATH
+    net_config = load_config(root_path, "config.yaml")
+    config = net_config["timelord_launcher"]
+    initialize_logging("masternode", config["logging"], DEFAULT_ROOT_PATH)
+    return asyncio.run(masternode_update_status_interval(selected_network, config))
 
 
 if __name__ == "__main__":
-    freeze_support()
     main()
