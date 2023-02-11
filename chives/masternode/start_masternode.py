dcc4b4f958d7f828c8c8be570feeebfad68a19ef
hivecoin2021@gmail.com <hivecoin2021@gmail.com>
2023-02-10 23:34:39 -0800

    start_masternode

--- chives/masternode/start_masternode.py
+++ chives/masternode/start_masternode.py
@@ -38,6 +38,7 @@ from chives.wallet.puzzles.p2_delegated_puzzle_or_hidden_puzzle import (  # stan
     calculate_synthetic_secret_key,
     DEFAULT_HIDDEN_PUZZLE_HASH,
 )
+from chives.masternode.masternode_manager import MasterNodeManager
 
 
 # See: https://bugs.python.org/issue29288
@@ -117,7 +118,19 @@ async def masternode_update_status_interval(selected_network, config) -> None:
                 log.warning('ChunkedEncodingError -- please wait 600 seconds............................')
             except:
                 log.warning('Unfortunitely -- An Unknow Error Happened, Please wait 600 seconds............................')
-
+            # update masternode data from blockchain
+            try:
+                manager = MasterNodeManager()
+                await manager.connect()
+                checkSyncedStatus, checkSyncedStatusText, fingerprint = await manager.checkSyncedStatus()
+                await manager.chooseWallet(fingerprint)
+                # for item in checkSyncedStatusText:
+                #    print(item)
+                if checkSyncedStatus == 2:
+                    await manager.sync_masternode_from_blockchain()
+                await manager.close()
+            except:
+                pass
         except:
             log.warning('Unfortunitely -- An Unknow except Happened, Please wait 600 seconds............................')
             pass
